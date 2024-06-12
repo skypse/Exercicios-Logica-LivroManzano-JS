89fc3bfade52de7c6bb15443cb1db65e4dbb2b64
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-12 11:53:32 -0300

    Update EX-SOMATORIO-NUMEROS-PARES.js

--- PAGINA-50/EX-B/EX-SOMATORIO-NUMEROS-PARES.js
+++ PAGINA-50/EX-B/EX-SOMATORIO-NUMEROS-PARES.js
@@ -1,13 +1,14 @@
 /* Elaborar um programa que apresente no final o somatório dos valores pares existentes na faixa de 
 1 até 500*/
 
-let numero = 1;
-let soma = 0;
+let contadora = 1;
+let acumuladora = 0;
 
-while (numero <= 500) {
-    if (numero % 2 === 0) {
-        soma += numero;
-    }
-    numero++;
-}
-console.log("A soma dos números pares é: " + soma);
+do {
+  if (contadora % 2 === 0) {
+    acumuladora += contadora;
+  }
+  contadora++;
+} while (contadora < 501);
+
+console.log("A soma dos números pares é: " + acumuladora);
