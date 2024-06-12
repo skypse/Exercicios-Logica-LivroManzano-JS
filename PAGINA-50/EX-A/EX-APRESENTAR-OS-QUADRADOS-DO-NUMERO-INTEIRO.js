7b1ed2efc32e19b853c3fa82d75e7dd06e5bcb62
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2024-06-12 10:58:05 -0300

    Update EX-APRESENTAR-OS-QUADRADOS-DO-NUMERO-INTEIRO.js

--- PAGINA-50/EX-A/EX-APRESENTAR-OS-QUADRADOS-DO-NUMERO-INTEIRO.js
+++ PAGINA-50/EX-A/EX-APRESENTAR-OS-QUADRADOS-DO-NUMERO-INTEIRO.js
@@ -1,7 +1,7 @@
 /*Apresentar os quadrados dos números inteiros de 15 a 200.*/
 
-let numero = 15;
-while (numero <= 200) {
-    console.log(numero * numero);
-    numero++;
-}
+let contadora = 15;
+do{
+    console.log(contadora * contadora);
+    contadora++;
+}while (contadora < 201) 
